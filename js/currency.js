0dfc74f1363a031a51913462d107cac8335c0aa9
Gyetvai <adamgyetvai26@gmail.com>
2024-11-27 09:05:26 +0100

    merge

--- js/currency.js
+++ js/currency.js
@@ -1,47 +1,67 @@
 const apiKey = 'd18cd56d330726aee31bf6db5dca5898';
-
 const apiUrl = `http://data.fixer.io/api/latest?access_key=${apiKey}`;
 
-//async function fetchExchangeRates() {
+//exchangable currencies
+const currencies = [
+    'HUF', 'EUR', 'USD', 'CHF', 'GBP', 'CZK', 
+    'JPY', 'SEK', 'AUD', 'NOK', 'CAD', 'RON', 
+    'PLN', 'RSD'
+];
 
+let data = {
+    HUF: 410.28409,
+    EUR: 1,
+    USD: 1.048487,
+    CHF: 0.927918,
+    GBP: 0.833495,
+    CZK: 25.201151,
+    JPY: 159.426165,
+    SEK: 11.50902,
+    AUD: 1.617562,
+    NOK: 11.687278,
+    CAD: 1.474593,
+    RON: 4.976962,
+    PLN: 4.304133,
+    RSD: 116.997561
+};
+
+// fetch if button pressed
+async function fetchExchangeRates() {
     try {
+        const response = await fetch(apiUrl);
+        const result = await response.json();
 
-        //const response = await fetch(apiUrl);
+        if (result.success) {
+            const exchangeRates = result.rates;
+            // update fetched dataes
+            data = currenciesToDisplay.reduce((filtered, currency) => {
+                if (exchangeRates[currency] !== undefined) {
+                    filtered[currency] = exchangeRates[currency];
+                }
+                return filtered;
+            }, {});
 
-        //const data = await response.json();
-        console.log(data);
-        if (data.success) {
-
-            //const exchangeRates = data.rates;
-            
-            const exchangeRatesDiv = document.getElementById('exchangeRates');
-
-            exchangeRatesDiv.innerHTML = '<h2>Exchange Rates:</h2><ul>';
-
-            for (const currency in exchangeRates) {
-
-                exchangeRatesDiv.innerHTML += `<li>${currency}: ${exchangeRates[currency]}</li>`;
-
-            }
-
-            exchangeRatesDiv.innerHTML += '</ul>';
-
+            updateExchangeRatesUI(data);
         } else {
-
-            console.error('Failed to fetch exchange rates:', 'http://data.error.info');
-
+            console.error('Failed to fetch exchange rates:', result.error);
         }
-
     } catch (error) {
-
         console.error('An error occurred while fetching exchange rates:', error);
-
     }
+}
 
-//}
+// function for currencie show
+function updateExchangeRatesUI(data) {
+    const exchangeRatesDiv = document.getElementById('exchangeRates');
+    exchangeRatesDiv.innerHTML = '<h2>Exchange Rates:</h2><ul>';
+    for (const [currency, rate] of Object.entries(data)) {
+        exchangeRatesDiv.innerHTML += `<li>${currency}: ${rate.toFixed(4)}</li>`;
+    }
+    exchangeRatesDiv.innerHTML += '</ul>';
+}
 
-fetchExchangeRates();
+//start fetch
+document.getElementById('refreshButton').addEventListener('click', fetchExchangeRates);
 
-// Refresh rates every 1 minute (adjust as needed)
-
-setInterval(fetchExchangeRates, 60000);
+//newest data show
+updateExchangeRatesUI(data);
