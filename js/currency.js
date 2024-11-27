6782d6210fd466d9dbab0042f5ce8b4551018950
Gyetvai <adamgyetvai26@gmail.com>
2024-11-27 08:28:48 +0100

    api

--- js/currency.js
+++ js/currency.js
@@ -2,43 +2,43 @@ const apiKey = 'd18cd56d330726aee31bf6db5dca5898';
 
 const apiUrl = `http://data.fixer.io/api/latest?access_key=${apiKey}`;
 
-async function fetchExchangeRates() {
+//async function fetchExchangeRates() {
 
-try {
+    try {
 
-const response = await fetch(apiUrl);
+        //const response = await fetch(apiUrl);
 
-const data = await response.json();
+        //const data = await response.json();
+        console.log(data);
+        if (data.success) {
 
-if (data.success) {
+            //const exchangeRates = data.rates;
+            
+            const exchangeRatesDiv = document.getElementById('exchangeRates');
 
-const exchangeRates = data.rates;
+            exchangeRatesDiv.innerHTML = '<h2>Exchange Rates:</h2><ul>';
 
-const exchangeRatesDiv = document.getElementById('exchangeRates');
+            for (const currency in exchangeRates) {
 
-exchangeRatesDiv.innerHTML = '<h2>Exchange Rates:</h2><ul>';
+                exchangeRatesDiv.innerHTML += `<li>${currency}: ${exchangeRates[currency]}</li>`;
 
-for (const currency in exchangeRates) {
+            }
 
-exchangeRatesDiv.innerHTML += `<li>${currency}: ${exchangeRates[currency]}</li>`;
+            exchangeRatesDiv.innerHTML += '</ul>';
 
-}
+        } else {
 
-exchangeRatesDiv.innerHTML += '</ul>';
+            console.error('Failed to fetch exchange rates:', 'http://data.error.info');
 
-} else {
+        }
 
-console.error('Failed to fetch exchange rates:', 'http://data.error.info');
+    } catch (error) {
 
-}
+        console.error('An error occurred while fetching exchange rates:', error);
 
-} catch (error) {
+    }
 
-console.error('An error occurred while fetching exchange rates:', error);
-
-}
-
-}
+//}
 
 fetchExchangeRates();
 
